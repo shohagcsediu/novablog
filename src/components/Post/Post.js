0d0986e955a35e94d2e29bff765d8b04883077ff
Mohammad Shohag <shohagcsediu@gmail.com>
2021-06-26 17:03:49 +0600

    reade more link added

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 
 const Post = (props) => {
-    const {id} = props.posts;
+    const {id, link} = props.posts;
     return (
         <Col border="primary" md={4} >
             <h3>{id}</h3>
-            
+            <a href = {`${link}`} target="_blank" rel="noreferrer">Reade More</a>
         </Col>
     );
 };
